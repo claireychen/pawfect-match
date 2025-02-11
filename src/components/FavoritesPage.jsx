032ec2a7f702e0249eb42401cf8535998e817d80
claireandj666 <claire.yandi.chen@gmail.com>
2025-02-10 22:46:21 -0500

    style favorites page

--- src/components/FavoritesPage.jsx
+++ src/components/FavoritesPage.jsx
@@ -6,41 +6,41 @@ function FavoritesPage({ favorites, removeFromFavorites }) {
   const navigate = useNavigate();
 
   return (
-    <div>
+		<header className="bg-pink shadow-sm">
+			<div className="text-center">
+				<h1 className="text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">🐾 Your Favorite Dogs</h1>
+				{favorites.length === 0 ? (
+					<p>No favorites found. Start searching your paw friends!</p>
+				) : (
+					<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
+						{favorites.map((dog) => (
+							<div key={dog.id} className="border rounded-lg p-4 shadow-md">
+								<img
+									src={dog.img}
+									alt={dog.name}
+									className="w-full h-48 object-cover rounded-md"
+								/>
+								<h3 className="text-lg font-bold mt-2">{dog.name}</h3>
+								<p>Breed: {dog.breed}</p>
+								<p>Age: {dog.age}</p>
+								<p>Location: {dog.zip_code}</p>
 
-      <h1>🐾 Your Favorite Dogs</h1>
-
-      {favorites.length === 0 ? (
-        <p>No favorites found. Start searching your paw friends!</p>
-      ) : (
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-4">
-          {favorites.map((dog) => (
-            <div key={dog.id} className="border rounded-lg p-4 shadow-md">
-              <img
-                src={dog.img}
-                alt={dog.name}
-                className="w-full h-48 object-cover rounded-md"
-              />
-              <h3 className="text-lg font-bold mt-2">{dog.name}</h3>
-              <p>Breed: {dog.breed}</p>
-              <p>Age: {dog.age}</p>
-              <p>Location: {dog.zip_code}</p>
-
-              <Button variant="destructive" onClick={() => removeFromFavorites(dog.id)}>
-                ❌ Remove
-              </Button>
-            </div>
-          ))}
-          <Button
-            variant="default"
-            className="mt-4 w-full"
-            onClick={() => navigate("/match", { state: { favoriteDogs: favorites }})}
-          >
-            🐶 Find My Match
-          </Button>
-        </div>
-      )}
-    </div>
+								<Button variant="destructive" onClick={() => removeFromFavorites(dog.id)}>
+									❌ Remove
+								</Button>
+							</div>
+						))}
+						<Button
+							variant="default"
+							className="mt-4 w-full"
+							onClick={() => navigate("/match", { state: { favoriteDogs: favorites }})}
+						>
+							🐶 Find My Match
+						</Button>
+					</div>
+				)}
+			</div>
+		</header>
   );
 }
 
