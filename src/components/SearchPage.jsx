d92854da33b9fd9d11020a3bd5d610261226f6a4
claireandj666 <claire.yandi.chen@gmail.com>
2025-02-11 23:42:51 -0500

    add more filters: age and zip code

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,13 +1,18 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
-import { Button } from "@material-tailwind/react";
+// import { Button } from "@material-tailwind/react";
 import DogList from './DogList';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function SearchPage({ addToFavorites }) {
 	const [breeds, setBreeds] = useState([]);
 	const [selectedBreed, setSelectedBreed] = useState('');
+	const [age, setAge] = useState('');
+	const [location, setLocation] = useState('');
 
 	useEffect(() => {
 		async function fetchBreeds() {
@@ -49,6 +54,16 @@ function SearchPage({ addToFavorites }) {
             </option>
           ))}
         </select>
+				<Box
+					component="form"
+					sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+					noValidate
+					autoComplete="off"
+				>
+					<TextField id="filled-basic" label="Age" variant="outlined" />
+					<TextField id="filled-basic" label="ZIP Code" variant="outlined" />
+					<Button variant="contained">Search</Button>
+				</Box>
 
 				{/* <Select
           className="mt-4 border p-2 rounded"
